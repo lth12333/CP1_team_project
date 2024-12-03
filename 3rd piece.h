67226e8f745534c15ae1d920cbb11e91cf6e30be
lth12333 <163487352+lth12333@users.noreply.github.com>
2024-12-04 01:04:55 +0900

    moving_point_count로 각 기물의 움직일 수 있는 타일 수 저장

---
+++ 3rd piece.h
@@ -0,0 +1,12 @@
+// piece.h
+#ifndef PIECE_H
+#define PIECE_H
+
+typedef struct {
+    char type;  // 'K' (King), 'Q' (Queen), 'R' (Rook), 'B' (Bishop), 'N' (Knight), 'P' (Pawn)
+    char color; // 'W' (White), 'B' (Black)
+    int warning;
+    int moving_point_count;
+} Piece;
+
+#endif
